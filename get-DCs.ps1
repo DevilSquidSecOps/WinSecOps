d61c4ab02ebf8340c94999d572ac6890682f13e7
Steven <darwinsgame@protonmail.com>
2019-09-05 10:35:04 -0700

    Create get-DCs.ps1

---
+++ get-DCs.ps1
@@ -0,0 +1,12 @@
+#ps1 script
+$dcs = Get-ADDomainController -Filter * | Select-Object -Property name #collect domain controllers in a var
+
+#loop through each one calling them by the object "name" filter we used in the above var
+foreach ($name in $dcs.name){
+  Test-Connection -ComputerName $name -Count 1 -ErrorAction Ignore
+  }
+
+# https://docs.microsoft.com/en-us/powershell/module/addsadministration/get-addomaincontroller?view=win10-ps
+#use a backtick ` char to escape special chars
+echo "`n primary DC is:"
+Get-ADDomainController -Discover -Domain "contoso.com" -Service "PrimaryDC"
